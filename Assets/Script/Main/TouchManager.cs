23ff54a802b79e7c7325b3e7d711527a7022d4fe
streetmeow <chojh106@gmail.com>
2020-01-19 20:04:38 +0900

    no message

---
+++ Assets/Script/Main/TouchManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchManager : MonoBehaviour
+    
+{
+    public bool isTouched { get; set; }
+
+    public void Start()
+    {
+        isTouched = false;
+    }
+
+    public void DownEvent()
+    {
+        isTouched = true;
+    }
+    public void UpEvent()
+    {
+        isTouched = false;
+    }
+}
--- README.md
+++
@@ -1,6 +0,0 @@
-# Space-Game
-Space game using unity
-
-Unity Version : 2019.2.18
-
-Language : C#
